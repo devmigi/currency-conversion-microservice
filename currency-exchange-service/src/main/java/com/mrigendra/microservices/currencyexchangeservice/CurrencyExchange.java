b10e12c54e635f8302feb9bda3ce2d5e647d97f0
Mrigendra <mrgndrakr@live.com>
2021-02-26 02:02:50 +0530

    currency-exchange-service h2 database and jpa added

--- currency-exchange-service/src/main/java/com/mrigendra/microservices/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/mrigendra/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -2,13 +2,26 @@ package com.mrigendra.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class CurrencyExchange {
 
+	@Id
 	private Long id;
-	private String from;
-	private String to;
-	private BigDecimal conversionMultiple;
 	
+	@Column(name = "currency_from")
+	private String from;
+	
+	@Column(name = "currency_to")
+	private String to;
+	
+	private BigDecimal conversionMultiple;
+	private String environment;
+
+
 	public CurrencyExchange() {
 		
 	}
@@ -59,6 +72,15 @@ public class CurrencyExchange {
 
 	public void setConversionMultiple(BigDecimal conversionMultiple) {
 		this.conversionMultiple = conversionMultiple;
+	}
+
+	
+	public String getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(String environment) {
+		this.environment = environment;
 	}
 	
 
--- currency-exchange-service/src/main/java/com/mrigendra/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/mrigendra/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.mrigendra.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private CurrencyExchangeRepository repo;
+	
 	@GetMapping(path = "/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange getCurrencyExchangeValue(@PathVariable String from, @PathVariable String to) {
-		return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(75));
+		
+		CurrencyExchange currencyExchange = repo.findByFromAndTo(from, to);
+		
+		if(currencyExchange == null) {
+			throw new RuntimeException("Unable to find data for " + from + " to " + to);
+		}
+		
+		String port = environment.getProperty("local.server.port");
+		currencyExchange.setEnvironment(port);
+		
+		
+		
+		return currencyExchange;
 	}
 
 }
