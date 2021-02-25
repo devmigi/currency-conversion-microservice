82a611edcbf44f5025367086678ccb2552149454
Mrigendra <mrgndrakr@live.com>
2021-02-26 02:26:02 +0530

    currency-conversion to currency-exchange http call added

--- currency-conversion-service/src/main/java/com/mrigendra/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/mrigendra/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,10 +1,13 @@
 package com.mrigendra.microservices.currencyconversionservice;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
@@ -15,8 +18,19 @@ public class CurrencyConversionController {
 			@PathVariable String to,
 			@PathVariable BigDecimal quantity
 			) {
+		HashMap<String, String> params = new HashMap<>();
+		params.put("from", from);
+		params.put("to", to);
 		
-		return new CurrencyConversion(1000L, from, to, BigDecimal.valueOf(50), quantity, BigDecimal.ONE, "");
+		ResponseEntity<CurrencyConversion> response = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", 
+				CurrencyConversion.class, params);
+		
+		CurrencyConversion currencyConversion = response.getBody();
+		
+		return new CurrencyConversion(currencyConversion.getId(), from, to, 
+				currencyConversion.getConversionMultiple(), quantity, 
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment());
 			
 	}
 }
