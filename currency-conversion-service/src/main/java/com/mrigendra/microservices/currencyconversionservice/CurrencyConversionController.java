3213683580761b40b10e13071c324694aedb6c0f
Mrigendra <mrgndrakr@live.com>
2021-02-26 20:30:06 +0530

    currency-conversion-service Feign library added to simplify rest calls

--- currency-conversion-service/src/main/java/com/mrigendra/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/mrigendra/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -3,6 +3,7 @@ package com.mrigendra.microservices.currencyconversionservice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,9 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class CurrencyConversionController {
 
+	@Autowired
+	private CurrencyExchangeProxy proxy;
+	
 	@GetMapping(path = "/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateConversion(
 			@PathVariable String from, 
@@ -31,6 +35,23 @@ public class CurrencyConversionController {
 				currencyConversion.getConversionMultiple(), quantity, 
 				quantity.multiply(currencyConversion.getConversionMultiple()), 
 				currencyConversion.getEnvironment());
+			
+	}
+	
+	
+	@GetMapping(path = "/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversion calculateConversionFeign(
+			@PathVariable String from, 
+			@PathVariable String to,
+			@PathVariable BigDecimal quantity
+			) {
+			
+		CurrencyConversion currencyConversion = proxy.getCurrencyExchangeValue(from, to);
+		
+		return new CurrencyConversion(currencyConversion.getId(), from, to, 
+				currencyConversion.getConversionMultiple(), quantity, 
+				quantity.multiply(currencyConversion.getConversionMultiple()), 
+				currencyConversion.getEnvironment() + " using Feign");
 			
 	}
 }
